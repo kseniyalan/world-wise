f51a3e3994ace4e14920852bbc3e3f341da49c3d
Kseniia Lanetskaia <kseniia.lanetskaia@jump-soft.com>
2024-04-08 10:05:52 +0200

    App Layout and components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+import SidebarFooter from "./SidebarFooter";
+
+function Sidebar() {
+    return (
+        <div className={styles.sidebar}>
+            <Logo />
+            <AppNav />
+            <p>List of cities</p>
+            <SidebarFooter />
+        </div>
+    );
+}
+
+export default Sidebar;
