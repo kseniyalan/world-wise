8e4da27cf32984f70f896f0b9714ad03a1e3d162
Kseniia Lanetskaia <kseniia.lanetskaia@jump-soft.com>
2024-04-08 23:30:52 +0200

    App Nav

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<div>Cities!!!</div>} />
+          <Route path="cities" element={<div>Cities</div>} />
+          <Route path="countries" element={<div>Countries</div>} />
+          <Route path="form" element={<div>Form</div>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
